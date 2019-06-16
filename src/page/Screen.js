b87e30e0513f8e573ad4dd82dc11fc323b68eace
Eunkang <choa030@gmai.com>
2019-06-17 01:30:30 +0900

    190614

--- src/page/Screen.js
+++ src/page/Screen.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 import data from "../data.json";
 import d3 from "d3";
 import "./Screen.scss";
@@ -12,7 +14,9 @@ import Effect7 from "../effects/Effect7";
 import Effect8 from "../effects/Effect8";
 
 import ReactDOM from "react-dom";
-import { readlink } from "fs";
+import {
+  readlink
+} from "fs";
 export class Screen extends React.Component {
   constructor(props) {
     super(props);
@@ -31,60 +35,24 @@ export class Screen extends React.Component {
         "#6D8B6D",
         "#E46460",
         "#EB9263",
-
         "#22c1c3",
-
         "#925D60"
 
-        // "#F78773",
-        // "#CD7FA5",
-        // "#C08EBD",
-        // "#AA9ED0",
-        // "#8FAEDC",
-        // "#6FBDE0",
-        // "#51CBDB",
-        // "#43D7CD",
-        // "#53E0BA",
-        // "#75E8A3",
-        // "#9BED8B",
-        // "#C4EF76",
-        // "#EFEE69"
-
-        // "#3366cc",
-        // "#dc3912",
-        // "#ff9900",
-        // "#109618",
-        // "#990099",
-        // "#0099c6",
-        // "#dd4477",
-        // "#66aa00",
-        // "#b82e2e",
-        // "#316395",
-        // "#994499",
-        // "#22aa99",
-        // "#aaaa11",
-        // "#6633cc",
-        // "#e67300",
-        // "#8b0707",
-        // "#651067",
-        // "#329262",
-        // "#5574a6",
-        // "#3b3eac"
       ];
       return _colors[n % _colors.length];
     }
 
     function position() {
-      this.style("left", function(d) {
-        return d.x + "%";
-      })
-        .style("top", function(d) {
+      this.style("left", function (d) {
+          return d.x + "%";
+        })
+        .style("top", function (d) {
           return d.y + "%";
         })
-        .style("width", function(d) {
+        .style("width", function (d) {
           return d.dx + "%";
         })
-        .style("height", function(d) {
+        .style("height", function (d) {
           return d.dy + "%";
         });
     }
@@ -97,7 +65,7 @@ export class Screen extends React.Component {
       .treemap()
       .size([100, 100])
       .sticky(true)
-      .value(function(d) {
+      .value(function (d) {
         return d.attendee;
       });
 
@@ -108,14 +76,14 @@ export class Screen extends React.Component {
       .data(treemap.nodes)
       .enter()
       .append("div")
-      .attr("class", function(d, i) {
+      .attr("class", function (d, i) {
         return "node node" + i;
       })
-      .attr("id", function(d, i) {
+      .attr("id", function (d, i) {
         return d.neighbor;
       })
       .call(position)
-      .style("background", function(d, i) {
+      .style("background", function (d, i) {
         return colors(i);
       })
       .text(getLabel);
@@ -130,74 +98,114 @@ export class Screen extends React.Component {
       switch (num) {
         // switch (num) {
         case 0:
-          return (
-            <Effect1
-              attendee={data.children[cityNum].children[neigborNum].neighbor}
-              names={data.children[cityNum].children[neigborNum].names}
-              word={data.children[cityNum].children[neigborNum].word}
+          return ( <
+            Effect1 attendee = {
+              data.children[cityNum].children[neigborNum].neighbor
+            }
+            names = {
+              data.children[cityNum].children[neigborNum].names
+            }
+            word = {
+              data.children[cityNum].children[neigborNum].word
+            }
             />
           );
           break;
         case 1:
-          return (
-            <Effect2
-              attendee={data.children[cityNum].children[neigborNum].neighbor}
-              names={data.children[cityNum].children[neigborNum].names}
-              word={data.children[cityNum].children[neigborNum].word}
+          return ( <
+            Effect2 attendee = {
+              data.children[cityNum].children[neigborNum].neighbor
+            }
+            names = {
+              data.children[cityNum].children[neigborNum].names
+            }
+            word = {
+              data.children[cityNum].children[neigborNum].word
+            }
             />
           );
           break;
         case 2:
-          return (
-            <Effect3
-              attendee={data.children[cityNum].children[neigborNum].neighbor}
-              names={data.children[cityNum].children[neigborNum].names}
-              word={data.children[cityNum].children[neigborNum].word}
+          return ( <
+            Effect3 attendee = {
+              data.children[cityNum].children[neigborNum].neighbor
+            }
+            names = {
+              data.children[cityNum].children[neigborNum].names
+            }
+            word = {
+              data.children[cityNum].children[neigborNum].word
+            }
             />
           );
           break;
         case 3:
-          return (
-            <Effect4
-              attendee={data.children[cityNum].children[neigborNum].neighbor}
-              names={data.children[cityNum].children[neigborNum].names}
-              word={data.children[cityNum].children[neigborNum].word}
+          return ( <
+            Effect4 attendee = {
+              data.children[cityNum].children[neigborNum].neighbor
+            }
+            names = {
+              data.children[cityNum].children[neigborNum].names
+            }
+            word = {
+              data.children[cityNum].children[neigborNum].word
+            }
             />
           );
           break;
         case 4:
-          return (
-            <Effect5
-              attendee={data.children[cityNum].children[neigborNum].neighbor}
-              names={data.children[cityNum].children[neigborNum].names}
-              word={data.children[cityNum].children[neigborNum].word}
+          return ( <
+            Effect5 attendee = {
+              data.children[cityNum].children[neigborNum].neighbor
+            }
+            names = {
+              data.children[cityNum].children[neigborNum].names
+            }
+            word = {
+              data.children[cityNum].children[neigborNum].word
+            }
             />
           );
           break;
         case 5:
-          return (
-            <Effect6
-              attendee={data.children[cityNum].children[neigborNum].neighbor}
-              names={data.children[cityNum].children[neigborNum].names}
-              word={data.children[cityNum].children[neigborNum].word}
+          return ( <
+            Effect6 attendee = {
+              data.children[cityNum].children[neigborNum].neighbor
+            }
+            names = {
+              data.children[cityNum].children[neigborNum].names
+            }
+            word = {
+              data.children[cityNum].children[neigborNum].word
+            }
             />
           );
           break;
         case 6:
-          return (
-            <Effect7
-              attendee={data.children[cityNum].children[neigborNum].neighbor}
-              names={data.children[cityNum].children[neigborNum].names}
-              word={data.children[cityNum].children[neigborNum].word}
+          return ( <
+            Effect7 attendee = {
+              data.children[cityNum].children[neigborNum].neighbor
+            }
+            names = {
+              data.children[cityNum].children[neigborNum].names
+            }
+            word = {
+              data.children[cityNum].children[neigborNum].word
+            }
             />
           );
           break;
         case 7:
-          return (
-            <Effect8
-              attendee={data.children[cityNum].children[neigborNum].neighbor}
-              names={data.children[cityNum].children[neigborNum].names}
-              word={data.children[cityNum].children[neigborNum].word}
+          return ( <
+            Effect8 attendee = {
+              data.children[cityNum].children[neigborNum].neighbor
+            }
+            names = {
+              data.children[cityNum].children[neigborNum].names
+            }
+            word = {
+              data.children[cityNum].children[neigborNum].word
+            }
             />
           );
           break;
@@ -220,22 +228,24 @@ export class Screen extends React.Component {
     console.log(divs);
   }
   render() {
-    return (
-      <div id="screen-con">
-        <div className="treemap" />
-        {/* {this.state.neighbors &&
-          this.state.neighbors.map(city => {
-            return city.children.map(neighbor => {
-              return (
-                <Effect1
-                  attendee={neighbor.neighbor}
-                  names={neighbor.names}
-                  word={neighbor.word}
-                />
-              );
-            });
-          })} */}
-      </div>
+    return ( <
+      div id = "screen-con" >
+      <
+      div className = "treemap" / > {
+        /* {this.state.neighbors &&
+                  this.state.neighbors.map(city => {
+                    return city.children.map(neighbor => {
+                      return (
+                        <Effect1
+                          attendee={neighbor.neighbor}
+                          names={neighbor.names}
+                          word={neighbor.word}
+                        />
+                      );
+                    });
+                  })} */
+      } <
+      /div>
     );
   }
 }
